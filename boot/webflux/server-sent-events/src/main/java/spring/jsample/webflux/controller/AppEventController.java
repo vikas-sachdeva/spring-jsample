6172a04b66a57b701b220f97653d0216ebb303f8
Vikas Sachdeva <techie.vikas.sachdeva@gmail.com>
2020-11-08 12:52:56 +0530

    server sent events

---
+++ boot/webflux/server-sent-events/src/main/java/spring/jsample/webflux/controller/AppEventController.java
@@ -0,0 +1,26 @@
+package spring.jsample.webflux.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import spring.jsample.webflux.model.Application;
+import spring.jsample.webflux.service.AppService;
+
+import java.time.Duration;
+
+@RestController
+public class AppEventController {
+
+    private AppService appService;
+
+    public AppEventController(AppService appService) {
+        this.appService = appService;
+    }
+
+    @GetMapping(value = "/app/status/{id}", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    public Flux<Application> subscribeNotifications(@PathVariable int id) {
+        return Flux.interval(Duration.ofSeconds(1)).map(a -> appService.getAppStatus(id));
+    }
+}
---
+++ boot/webflux/server-sent-events/src/main/java/spring/jsample/webflux/model/Application.java
@@ -0,0 +1,68 @@
+package spring.jsample.webflux.model;
+
+import java.util.Objects;
+import java.util.StringJoiner;
+
+public class Application {
+
+    private int id;
+
+    private String name;
+
+    private String status;
+
+    public Application(int id, String name, String status) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Application that = (Application) o;
+        return id == that.id &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(status, that.status);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, status);
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Application.class.getSimpleName() + "[", "]")
+                .add("id=" + id)
+                .add("name='" + name + "'")
+                .add("status='" + status + "'")
+                .toString();
+    }
+}
